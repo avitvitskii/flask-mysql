2e5233cb3faae506d920917a8ae6dc7e9a49c853
Anton Vitvitskii <avitvitskii@flightpath3d.com>
2020-11-20 19:46:43 +0300

    Adds form for adding new rows to db

--- app.py
+++ app.py
@@ -6,47 +6,49 @@ import json
 app = Flask(__name__)
 
 
-# def get_db():
-#     if not hasattr(g, 'db'):
-#         config = {
-#             'user': 'root',
-#             'password': 'somepass',
-#             'host': 'db',
-#             "port": '3306',
-#             "database": 'database'
-#         }
-#         g.db = mysql.connector.connect(**config)
-#     return g.db
+def get_db():
+    if not hasattr(g, 'db'):
+        config = {
+            'user': 'root',
+            'password': 'somepass',
+            'host': 'db',
+            "port": '3306',
+            "database": 'database'
+        }
+        g.db = mysql.connector.connect(**config)
+    return g.db
 
 
-# @app.teardown_appcontext
-# def close_db(error):
-#     if hasattr(g, 'db'):
-#         g.db.close()
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 
 @app.route('/')
-def hello_world():
-    config = {
-        'user': 'root',
-        'password': 'somepass',
-        'host': 'db',
-        "port": '3306',
-        "database": 'database'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor(buffered=True, dictionary=True)
+def show_students():
+    cursor = get_db().cursor(buffered=True, dictionary=True)
     cursor.execute('SELECT * FROM students')
     rows = []
     for row in cursor.fetchall():
         rows.append({
             'id': row['id'],
             'name': row['name'],
-            'created_at': humanize.naturaltime(row['created_at'])
+            'created_at': row['created_at']
         })
     cursor.close()
-    connection.close()
     return render_template('index.html', rows=rows)
+
+
+@app.route('/add', methods=["POST"])
+def add_post():
+    db = get_db()
+    cur = db.cursor()
+    sql = "INSERT students(name) VALUES (%s)"
+    value = (request.form['student'],)
+    cur.execute(sql, value)
+    db.commit()
+    return redirect(url_for('show_students'))
 
 
 if __name__ == '__main__':
